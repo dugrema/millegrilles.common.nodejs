614b6b58c881dc9eabefe896a5d5ac02e7fd3f58
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-11-14 14:55:42 -0500

    Ajout projet react downloadmanager

---
+++ playground/downloadmanager/README.md
@@ -0,0 +1,3 @@
+# File transfer manager
+
+Exemple d'upload et download
---
+++ playground/downloadmanager/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Test de upload et download manager</h1>
+
+      <h2>Upload manager</h2>
+
+      <h2>Download manager</h2>
+
+    </div>
+  );
+}
+
+export default App;
